2117da7e896e779e9fe3546112ccde6e028ed7b6
zhibi <zhibi@local.dev>
2026-06-09 15:24:19 +0800

    feat: add Tauri updater plugin for auto-update

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,6 +7,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_sql::Builder::new().build())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .setup(|app| {
             db::init_app_db(app.handle())?;
             Ok(())
